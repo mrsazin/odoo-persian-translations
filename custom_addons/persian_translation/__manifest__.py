3bf8806bada559036a30351a9227c6af2cc77bd8
mrsazin <145364382+mrsazin@users.noreply.github.com>
2024-07-06 10:10:51 +0330

    aded new module for persian translation

---
+++ custom_addons/persian_translation/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'persian_fa',
+    'version': '1.0',
+    'category': 'Localization',
+    'summary': 'Extra Persian translations ',
+    'description': 'This module contains custom translations for persian/farsi.',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
+
