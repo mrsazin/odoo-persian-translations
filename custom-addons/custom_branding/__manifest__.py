84ea9e774cd026ff7e9c39eb7558d7fbcfda4ca7
A.Na <hsadatoma@gmail.com>
2024-09-22 12:26:30 +0330

    add branign module

---
+++ custom-addons/custom_branding/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Custome Branding',
+    'summary': 'Customizes the logos and branding of Odoo' ,
+    'category': 'Customization',
+    'version': '1.0',
+    'author':'Azar Nazari (azar.nazari@gmail.com)',
+    'description': """
+RG Branding
+========================
+
+This module Changes all Odoo's logos of the Odoo Web Client.
+""",
+    'depends': ['web'],
+    'data': [ 
+        '/views/webclient_templates.xml'
+             ],
+    'assets': {
+        'web.assets_backend': [
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/img',
+            'custom_branding/static/src/webclient/webclient.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
