b330dd535695968f74f65a29d83e3e93b804352c
A.Na <hsadatoma@gmail.com>
2024-10-05 16:03:04 +0330

    some improvment

---
+++ custom-addons/l10n_fa_hr_org_chart/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Persian HR_org_chart',
+    'summary': 'Improved persian translation for hr' ,
+    'category': 'Localization',
+    'version': '1.0',
+    'author':'Azar Nazari (azar.nazari@gmail.com)',
+    'depends': ['hr_org_chart'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
