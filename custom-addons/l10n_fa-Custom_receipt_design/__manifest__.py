b21c4de63bb79c179eb09df3459f60be45a3da8c
A.Na <hsadatoma@gmail.com>
2024-10-18 17:06:43 +0330

    some improvment

---
+++ custom-addons/l10n_fa-Custom_receipt_design/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Persian translation for custom receipt design',
+    'summary': 'Improved persian translation for custom receipt design' ,
+    'category': 'Localization',
+    'version': '1.0',
+    'author':'Azar Nazari (azar.nazari@gmail.com)',
+    'depends': ['custom_receipt_design'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
